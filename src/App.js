4f8ddcb485e95cad50e38f22b11856173e2b6a25
maluyan <maluyan@cn-software.net>
2023-02-02 13:16:18 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Form, Button, Input, Modal, } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Modal title="添加" open footer={null}>
+        <Form>
+          <Form.Item label="姓名" name="name" help="长度在20个字符以内" rules={[{ required: true, message: '请输入姓名' }, { max: 20 }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="手机号" name="phone">
+            <Input />
+          </Form.Item>
+          <Form.Item>
+            <Button htmlType="reset">重置</Button>
+            <Button type="primary" htmlType="submit">提交</Button>
+          </Form.Item>
+          <div>点击”重置“按钮后，”姓名“表单的高度会发生变化</div>
+        </Form>
+      </Modal>
     </div>
   );
 }
